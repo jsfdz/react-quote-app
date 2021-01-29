851c31e994f6c66faa034c34e9349a106cf75a32
CrisanFdez <crisanfdez@gmail.com>
2021-01-29 14:18:24 -0500

    add fontawesome icons

--- src/components/QuotesBox.js
+++ src/components/QuotesBox.js
@@ -1,19 +1,26 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faQuoteLeft, faQuoteRight } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 export const QuotesBox = ({ quote, author, onQuoteChange }) => {
 
     return (
         <div className='quoteBox'>
-            <h1 className='quote'>{quote}</h1>
-            <h3 className='author'>- {author}</h3>
+            <blockquote>
+                <FontAwesomeIcon icon={faQuoteLeft} />
+                <h1 className='quote'>{quote}</h1>
+                <FontAwesomeIcon icon={faQuoteRight} />
+                <h3 className='author'>- {author}</h3>
+            </blockquote>
             <div className='btns'>
                 <a
-                    className='btn'
+                    className='btn tweet'
                     href={`https://twitter.com/intent/tweet?text=${quote}-${author}`}
                     target='_blank'
                     rel='noopener noreferrer'
                 >
-                    Tweet
+                    <FontAwesomeIcon icon={faTwitter} />
                 </a>
                 <button className='btn' onClick={onQuoteChange}>
                     New Quote
